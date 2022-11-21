a1d149af3c00a313d9f673deb30cc1f2545a7459
fillipe <fillipecool@gmail.com>
2022-11-21 17:13:45 -0300

    teste

--- src/components/myChat.vue
+++ src/components/myChat.vue
@@ -1,28 +1,23 @@
 <template>
-    <textarea @keydown.enter="getSentimental(), getWord()" type="text"  v-model="msg" placeholder="Coloque um texto" class="z-40 absolute w-96 h-60 border-2"/> 
-    <div contenteditable="true" class="absolute w-96 h-60 bg-red-500 text-transparent bg border-neutral-800"> 
-    {{ msg }}
-    </div> 
+    <textarea @keydown.enter="getSentimental(), getWord()" type="text"  v-model="msg" placeholder="Coloque um texto" class=" z-40 w-96 h-40 border-2"/> 
+    <ul v-for="info in info" :key="info" class="w-60 bg-red-500 bg border-neutral-800"> 
+    <li type="button" onclick="getCorrect()" v-if="info != null">{{ info }}</li>
+    </ul> 
 </template>
 
+
 <script>
-
 import axios from "axios"
-
 export default {
   name: 'myChat',
     data() {
       return {
         msg: "",
         erros: "",
-        suggestions: {
-          type: [Array, Object],
-          default() {
-            return []
-          }
+        info: [],
+        suggestions: []
         }
-      }
-    },
+      },
   methods: {
     getSentimental(){
       try{
@@ -38,20 +33,24 @@ export default {
       try{
         for (let index = 0; index < palavras.length; index++){
           axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+palavras[index])
-          .then((response) =>{
-              response.data.matches.length != 0 ? this.getCorrect(response.data.matches[0].sentence) : ''
+          .then((response) => {
+            console.log('aqui', response.data.matches[0].sentence)
+            if(response.data.matches.length !== 0 && response.data.matches[0].sentence !== null){
+              this.info[index] = response.data.matches[0].sentence
+            }
+              console.log(this.info)
           })
         }
       }catch(error){
         console.log(error)
       }
-    
     },
-    getCorrect(param){
-
-      console.log('chegou', param)
-
+    getCorrect(){
+      
     },
+    // getReplacement(){
+    //   this.getCorrect()
+    // }
   },
 }
 </script>
