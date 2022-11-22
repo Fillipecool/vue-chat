a12675307390a08599b290188bb24ef27493308e
fillipe <fillipecool@gmail.com>
2022-11-22 18:00:20 -0300

    teste

--- src/components/myChat.vue
+++ src/components/myChat.vue
@@ -1,13 +1,18 @@
 <template>
     <textarea @keydown.enter="getSentimental(), getWord()" type="text"  v-model="msg" placeholder="Coloque um texto" class=" z-40 w-96 h-40 border-2"/> 
-    <ul v-for="info in info" :key="info" class="w-60 bg-red-500 bg border-neutral-800"> 
-    <li type="button" onclick="getCorrect()" v-if="info != null">{{ info }}</li>
-    </ul> 
+
+    <div v-for="info in info" :key="info" class="w-60 bg-red-500 bg border-neutral-800"> 
+      <button id="corretor" type="button" @click="getCorrect(suggestions)" v-if="info != null">{{ info }}</button>
+      <span class="border-2"> {{ suggestions }} </span>
+    </div> 
+    
+    <!-- <button v-show="" type="button"> {{ suggestions }} </button> -->
 </template>
 
 
 <script>
 import axios from "axios"
+
 export default {
   name: 'myChat',
     data() {
@@ -15,42 +20,52 @@ export default {
         msg: "",
         erros: "",
         info: [],
-        suggestions: []
+        suggestions:[]
         }
       },
   methods: {
+    /**
+     * getSentimental irá verificar o sentimento
+     * da FRASE para entender se é negativa, Neutra ou Positiva
+     */
     getSentimental(){
-      try{
-        axios.get("https://test-nlp-api.alertrack.com.br/v1/polarity/unique/?sentence="+this.msg).then((response) => {
+        axios.get("https://test-nlp-api.alertrack.com.br/v1/polarity/unique/?sentence="+this.msg)
+        .then((response) => {
         console.log(response.data.describe)
       })
-      }catch(error){
-        console.log(error)
-      }
+      .catch(error => console.log(error))
     },
+
+    /**
+     * getWord verificará cada palavra errada
+     * separada por espaço
+     * e devolverá a requisição das erradas para serem corrigidas
+     */
     getWord(){
       const palavras = this.msg.split(' ')
-      try{
         for (let index = 0; index < palavras.length; index++){
           axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+palavras[index])
           .then((response) => {
-            console.log('aqui', response.data.matches[0].sentence)
+            // console.log('aqui', response.data.matches[0].sentence)
             if(response.data.matches.length !== 0 && response.data.matches[0].sentence !== null){
               this.info[index] = response.data.matches[0].sentence
             }
-              console.log(this.info)
           })
+          .catch(error => console.log(error))
         }
-      }catch(error){
-        console.log(error)
-      }
     },
     getCorrect(){
-      
+      const correto = this.msg.split(' ')
+        for (let index = 0; index < correto.length; index++){
+          axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+correto[index])
+          .then((response) => {
+            // console.log('aqui', response.data.matches[0])
+              this.suggestions = response.data.matches[0].replacements
+              console.log(this.suggestions)
+          })
+          .catch(error => console.log(error))
+        }
     },
-    // getReplacement(){
-    //   this.getCorrect()
-    // }
   },
 }
 </script>
