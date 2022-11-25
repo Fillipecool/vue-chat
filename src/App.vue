e0a9ff7a143f09bea5d5bde838c6945ceb824aa4
fillipe <fillipecool@gmail.com>
2022-11-25 17:06:37 -0300

    mudança estrutura

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
     <myChat />
-    <inputComponent />
 </template>
 
 <script>
   import myChat from './components/myChat.vue'
-  import inputComponent from './components/inputComponent.vue';
   export default {
     name: 'App',
     components: {
       myChat,
-      inputComponent
     }
   }
 </script>
--- src/components/myChat.vue
+++ src/components/myChat.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="w-4/5 h-48  absolute bottom-10 left-10">
-      <input @keydown.enter="getSentimental(), getWord()" v-model="msg" class="absolute placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md z-40 w-4/5 h-48 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm text-start" placeholder="Escreva alguma coisa" type="text" name="textchat"/>
+      <textarea v-model="msg" rows="10" cols="125" @keydown.enter="getSentimental(), getWord()" class="resize-none absolute placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md z-40 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm text-start" placeholder="Escreva alguma coisa" type="text" name="textchat"/>
     </div>
-    <div v-for="info in info" :key="info" class="w-60 bg-red-500 bg border-neutral-800"> 
-      <button id="corretor" type="button" @click="getCorrect()" v-if="info != null"> 
+    <div class="bg-red-500 bg border-neutral-800"> 
+      <button id="corretor" type="button" @click="getCorrect()" v-if="info != 0"
+      > 
         {{ info }}
       </button> 
     </div> 
-    <div v-for="suggestion in suggestions" :key="suggestion">
-      <button id="replace" type="button" v-if="suggestions != 0" class="border-2 no-underline hover:underline"> 
+    <div v-for="(suggestions, index) in suggestions" :key="index">
+      <button id="replace" type="button" v-if="suggestions != 0" class="border-2 no-underline hover:underline"
+      > 
         {{ suggestions }}
       </button>
     </div>
@@ -20,13 +22,13 @@ import axios from "axios"
 
 export default {
   name: 'myChat',
+
     setup(){
 
     },
     data() {
       return {
         msg: "",
-        erros: "",
         info: [],
         suggestions:[]
         }
@@ -49,31 +51,31 @@ export default {
      * separada por espaço
      * e devolverá a requisição das erradas para serem corrigidas
      */
-    getWord(){
-      const palavras = this.msg.split(' ')
-        for (let index = 0; index < palavras.length; index++){
-          axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+palavras[index])
+    getWord(){{
+          axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+this.msg)
           .then((response) => {
             // console.log('aqui', response.data.matches[0].sentence)
             if(response.data.matches.length !== 0 && response.data.matches[0].sentence !== null){
-              this.info[index] = response.data.matches[0].sentence
+              this.info = response.data.matches[0].sentence
+              console.log(response.data.matches[0])
             }
           })
           .catch(error => console.log(error))
         }
     },
+
+    treatmentIndo(){
+  
+    },
+
     getCorrect(){
-      const correto = this.msg.split(' ')
-        for (let index = 0; index < correto.length; index++){
-          axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+correto[index])
+          axios.get('https://test-languagetools.alertrack.com.br/v2/check?language=pt-BR&text='+this.msg)
           .then((response) => {
-            // console.log('aqui', response.data.matches[0])
               this.suggestions = response.data.matches[0].replacements
               console.log("sugestão", this.suggestions)
           })
           .catch(error => console.log(error))
         }
     },
-  },
-}
+  }
 </script>
